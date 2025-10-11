e74b33754931552487424e18e482dfcd79935faf
hitoya <nokimberly74@gmail.com>
2025-10-11 18:01:55 +0800

    first commit

---
+++ lua/core/options.lua
@@ -0,0 +1,16 @@
+vim.wo.number = true -- make like numbers show
+vim.o.relativenumber = false
+vim.o.clipboard = "unnamedplus"
+vim.o.wrap = false
+vim.o.linebreak = true
+vim.o.mouse = 'a'
+vim.o.autoindent = true
+
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+
+vim.o.showmode = true
+
+
